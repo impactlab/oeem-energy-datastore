89bb4dbcd89312ecc1a201fe8b831d3616297219
hunterowens <owens.hunter@gmail.com>
2015-09-09 16:41:20 -0500

    partial implementation of #3

--- oeem_energy_datastore/settings.py
+++ oeem_energy_datastore/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'oauth2_provider',
     'rest_framework',
     'datastore',
+    'django_pdb',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -53,6 +54,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django_pdb.middleware.PdbMiddleware',
 )
 
 ROOT_URLCONF = 'oeem_energy_datastore.urls'
