8d8531eebb25aff91c838109280792d2a4e292a4
Ubuntu <ubuntu@ip-172-31-37-218.us-west-2.compute.internal>
2015-09-23 17:09:53 +0000

    add static root

--- oeem_energy_datastore/settings.py
+++ oeem_energy_datastore/settings.py
@@ -101,6 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/home/ubuntu/static/"
 
 OAUTH2_PROVIDER = {
     'SCOPES': {'read': 'Read scope', 'write': 'Write scope'},
