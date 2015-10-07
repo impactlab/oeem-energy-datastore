d218cf5067d2e5ea6c01869d7448868939eabf53
Phil Ngo <ngo.phil@gmail.com>
2015-10-07 16:19:38 -0500

    Add new models to admin

--- datastore/admin.py
+++ datastore/admin.py
@@ -9,6 +9,13 @@ from .models import DailyUsageBaseline
 from .models import DailyUsageReporting
 from .models import MonthlyAverageUsageBaseline
 from .models import MonthlyAverageUsageReporting
+from .models import MonthlyUsageSummaryBaseline
+from .models import MonthlyUsageSummaryActual
+from .models import MonthlyUsageSummaryReporting
+from .models import DailyUsageSummaryBaseline
+from .models import DailyUsageSummaryActual
+from .models import DailyUsageSummaryReporting
+from .models import FuelTypeSummary
 
 admin.site.register(ProjectOwner)
 admin.site.register(Project)
@@ -20,5 +27,12 @@ admin.site.register(DailyUsageBaseline)
 admin.site.register(DailyUsageReporting)
 admin.site.register(MonthlyAverageUsageBaseline)
 admin.site.register(MonthlyAverageUsageReporting)
+admin.site.register(MonthlyUsageSummaryBaseline)
+admin.site.register(MonthlyUsageSummaryActual)
+admin.site.register(MonthlyUsageSummaryReporting)
+admin.site.register(DailyUsageSummaryBaseline)
+admin.site.register(DailyUsageSummaryActual)
+admin.site.register(DailyUsageSummaryReporting)
+admin.site.register(FuelTypeSummary)
 
 admin.site.site_header = "Open Energy Efficiency Meter"
