ad382e9d8a762e986e9c1e58712e36eb070ae8f2
Phil Ngo <ngo.phil@gmail.com>
2016-01-21 23:49:18 +0000

    Disable celery worker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,14 +9,14 @@ else
 	python manage.py collectstatic --noinput
 fi
 
-mkdir /srv/logs/celery
 mkdir /srv/run/celery
+mkdir /srv/logs/celery
 
-exec celery multi start worker1 \
-    -A oeem_energy_datastore \
-    -l info \
-    --pidfile="/srv/run/celery/%n.pid" \
-    --logfile="/srv/logs/celery/%n.log" \
+# exec celery multi start worker1 \
+#     -A oeem_energy_datastore \
+#     -l info \
+#     --pidfile="/srv/run/celery/%n.pid" \
+#     --logfile="/srv/logs/celery/%n.log" \
 
 touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
