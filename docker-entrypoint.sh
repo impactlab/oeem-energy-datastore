10742e58232e49462c72175a5da2423499522739
Phil Ngo <ngo.phil@gmail.com>
2016-01-22 00:03:47 +0000

    Add debugging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,11 +12,9 @@ fi
 mkdir /srv/run/celery
 mkdir /srv/logs/celery
 
-exec celery multi start worker1 \
-    -A oeem_energy_datastore \
-    -l info \
-    --pidfile="/srv/run/celery/%n.pid" \
-    --logfile="/srv/logs/celery/%n.log"
+exec celery multi start worker1 -A oeem_energy_datastore -l info --pidfile="/srv/run/celery/%n.pid" --logfile="/srv/logs/celery/%n.log"
+
+tail /srv/logs/celery/worker1.log
 
 touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
