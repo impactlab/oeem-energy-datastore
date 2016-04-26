2afcfd5de4097528385cae13f7c988d408a0ee9d
Phil Ngo <ngo.phil@gmail.com>
2016-04-26 10:38:37 -0700

    Start celery workers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,7 @@ fi
 mkdir /srv/run/celery
 mkdir /srv/logs/celery
 
-# exec celery multi start worker1 -A oeem_energy_datastore -l info --pidfile="/srv/run/celery/%n.pid" --logfile="/srv/logs/celery/%n.log"
+celery multi start 3 -A oeem_energy_datastore -l info --pidfile="/srv/run/celery/%n.pid" --logfile="/srv/logs/celery/%n.log"
 
 touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
