75031595de8726dcd21535b13385c4e6c89aa190
Phil Ngo <ngo.phil@gmail.com>
2016-04-26 14:28:15 -0700

    Add run meter task

---
+++ datastore/tasks.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from celery import shared_task
+
+from datastore.models import Project
+
+
+@shared_task
+def run_meter(project_pk):
+    project = Project.objects.get(pk=project_pk):
+    project.run_meter()
