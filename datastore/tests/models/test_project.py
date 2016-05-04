3dc2fa285954f06436b87f77f3eaf433437a8513
Phil Ngo <ngo.phil@gmail.com>
2016-05-04 10:24:28 -0700

    Add tests for project meter runs

--- datastore/tests/models/test_project.py
+++ datastore/tests/models/test_project.py
@@ -8,6 +8,9 @@ from datetime import datetime, timedelta
 import eemeter.project
 import eemeter.evaluation
 
+import numpy as np
+from numpy.testing import assert_allclose
+
 import pytz
 
 class ProjectTestCase(TestCase):
@@ -33,20 +36,41 @@ class ProjectTestCase(TestCase):
             longitude=0,
         )
 
-        consumptionmetadata = models.ConsumptionMetadata.objects.create(
+        cm_ng = models.ConsumptionMetadata.objects.create(
+            project=self.complete_project,
+            fuel_type="NG",
+            energy_unit="THM",
+        )
+
+        cm_e = models.ConsumptionMetadata.objects.create(
             project=self.complete_project,
             fuel_type="E",
             energy_unit="KWH",
         )
 
         for i in range(0, 700, 30):
+            if i % 120 == 0:
+                value = np.nan
+            else:
+                value = 1.0
             models.ConsumptionRecord.objects.create(
-                metadata=consumptionmetadata,
+                metadata=cm_ng,
                 start=datetime(2011, 1, 1, tzinfo=pytz.UTC) + timedelta(days=i),
-                value=1.0,
+                value=value,
                 estimated=False,
             )
 
+        for i in range(0, 700, 1):
+            if i % 4 == 0:
+                value = np.nan
+            else:
+                value = 1
+            models.ConsumptionRecord.objects.create(
+                metadata=cm_e,
+                start=datetime(2012, 1, 1, tzinfo=pytz.UTC) + timedelta(seconds=i*900),
+                value=value,
+                estimated=False,
+            )
 
     def test_attributes(self):
         attributes = [
@@ -99,7 +123,7 @@ class ProjectTestCase(TestCase):
 
         complete_eemeter_project, cm_ids = self.complete_project.eemeter_project()
         assert isinstance(complete_eemeter_project, eemeter.project.Project)
-        assert len(cm_ids) == 1
+        assert len(cm_ids) == 2
 
     def test_run_meter_recent_meter_runs(self):
 
@@ -114,7 +138,7 @@ class ProjectTestCase(TestCase):
 
         meter_run = meter_runs[0]
         assert isinstance(meter_run[0], int)
-        assert meter_run[1][0]["fuel_type"] == "E"
+        assert meter_run[1][0]["fuel_type"] == "NG"
         assert meter_run[1][0]["meterrun"] is not None
 
         self.complete_project.run_meter()
@@ -123,3 +147,18 @@ class ProjectTestCase(TestCase):
         assert len(meter_runs) == 1
 
         self.complete_project.meterrun_set.all().delete()
+
+    def test_run_meter(self):
+        meter_runs = models.Project.recent_meter_runs()
+        assert meter_runs == {}
+
+        self.complete_project.run_meter()
+
+        meter_runs = list(models.Project.recent_meter_runs().items())
+        assert len(meter_runs) == 2
+
+        meter_run = meter_runs[0][1][0]["meterrun"]
+        assert_allclose(meter_run.annual_savings, 0, rtol=1e-3, atol=1e-3)
+        assert_allclose(meter_run.gross_savings, 0, rtol=1e-3, atol=1e-3)
+        assert_allclose(meter_run.cvrmse_baseline, 0, rtol=1e-3, atol=1e-3)
+        assert_allclose(meter_run.cvrmse_reporting, 0, rtol=1e-3, atol=1e-3)
