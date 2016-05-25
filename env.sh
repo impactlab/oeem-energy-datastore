89acf34cbe75c53c3aca4261904b3ebe62ed7c77
Phil Ngo <ngo.phil@gmail.com>
2016-05-25 13:47:15 -0700

    Add production config

--- env.sh
+++ env.sh
@@ -3,6 +3,7 @@
 export DATABASE_URL=postgres://$(cat /etc/secret-volume/pg-user):$(cat /etc/secret-volume/pg-password)@$(cat /etc/secret-volume/pg-service-name).default.svc.cluster.local:5432/$(cat /etc/secret-volume/pg-user)
 export SECRET_KEY=$(cat /etc/secret-volume/secret-key)
 export DJANGO_LOGFILE=/srv/logs/django.log
+export CELERY_LOGFILE=/srv/logs/celery.log
 export BROKER_URL=amqp://guest:guest@$(cat /etc/secret-volume/rabbitmq-service-name).default.svc.cluster.local/
 export SERVER_NAME=$(cat /etc/secret-volume/server-name)
 export BROKER_TRANSPORT=amqp
