8d416ce4c814828a03c992d076e2ba355b9ef6e2
Marc Pare <marcpare0@gmail.com>
2016-06-02 10:19:53 -0700

    Output some real stats about Projects and Consumption data in db

--- portal/views.py
+++ portal/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+
+    # TODO: move into a model or service object or ...
+    from datastore import models
+    data = {
+        'project_count': models.Project.objects.count(),
+        'consumptionmetadata_count': models.ConsumptionMetadata.objects.count(),
+        'consumptionrecord_count': models.ConsumptionRecord.objects.count()
+    }
+
+    return render(request, 'index.html', data)
 
 def project_runs(request, id=None):
     """Render the table of results for a ProjectRun in an html table"""
@@ -9,7 +18,6 @@ def project_runs(request, id=None):
     # TODO: move into a model or service object or ...
     from datastore import models
     meter_runs = models.MeterRun.objects.all().prefetch_related("project", "consumption_metadata")
-
     data = {'meter_runs': meter_runs}
 
     return render(request, 'project_run_table.html', data)
