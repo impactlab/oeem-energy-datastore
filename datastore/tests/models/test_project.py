Update dev_seed with trace data and fix small bug on meter derivative saving.

datastore/tests/models/test_project.py
@@ -16,6 +16,8 @@ from numpy.testing import assert_allclose
 import pytz
 
 from datastore import models
+from datastore.services import create_project
+
 
 class ProjectTestCase(TestCase):
 
@@ -24,58 +26,77 @@ class ProjectTestCase(TestCase):
 
         cls.user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
 
-        cls.project = models.Project.objects.create(
-            project_owner=cls.user.projectowner,
-            project_id="PROJECTID_1",
-            baseline_period_start=None,
-            baseline_period_end=datetime(2012, 1, 1, tzinfo=pytz.UTC),
-            reporting_period_start=datetime(2012, 1, 2, tzinfo=pytz.UTC),
-            reporting_period_end=None,
-            zipcode="91104",
-            weather_station="722880",
-            latitude=0,
-            longitude=0,
-        )
+        cls.project = create_project(spec={
+            "project_id": "ABCD",
+            "project_owner": cls.user.projectowner,
+            "baseline_period_end": datetime(2012, 1, 1, tzinfo=pytz.UTC),
+            "reporting_period_start": datetime(2012, 2, 1, tzinfo=pytz.UTC),
+            "zipcode": "91104",
+            "traces": [
+                {
+                    "interpretation": "NG_C_S",
+                    "unit": "THM",
+                    "start": "2010-01-01",
+                    "end": "2014-12-31",
+                    "freq": "MS",
+                    "value": 1,
+                    "nans": set(range(0, 60, 20)),
+                    "estimated": set(range(3, 60, 15)),
+                },
+                {
+                    "interpretation": "NG_C_S",
+                    "unit": "THM",
+                    "start": "2011-09-01",
+                    "end": "2014-12-31",
+                    "freq": "D",
+                    "value": 2,
+                    "nans": set(range(0, 1000, 20)),
+                    "estimated": set(range(3, 1000, 15)),
+                },
+                {
+                    "interpretation": "E_C_S",
+                    "unit": "KWH",
+                    "start": "2011-01-01",
+                    "end": "2014-12-31",
+                    "freq": "15T",
+                    "value": 0.04,
+                    "nans": set(range(0, 96*365*4, 200)),
+                    "estimated": set(range(3, 96*365*4, 150)),
+                },
+                {
+                    "interpretation": "E_C_S",
+                    "unit": "KWH",
+                    "start": "2011-01-01",
+                    "end": "2014-12-31",
+                    "freq": "H",
+                    "value": 0.4,
+                    "nans": set(range(0, 96*365*4, 200)),
+                    "estimated": set(range(3, 96*365*4, 150)),
+                },
+                {
+                    "interpretation": "E_OSG_U",
+                    "unit": "KWH",
+                    "start": "2012-01-15",
+                    "end": "2014-12-31",
+                    "freq": "H",
+                    "value": 0.3,
+                    "nans": set(range(0, 96*365*4, 200)),
+                    "estimated": set(range(3, 96*365*4, 150)),
+                },
+                {
+                    "interpretation": "E_OSG_U",
+                    "unit": "KWH",
+                    "start": "2010-01-01",
+                    "end": "2014-12-31",
+                    "freq": "30T",
+                    "value": 0.1,
+                    "nans": set(range(0, 96*365*4, 200)),
+                    "estimated": set(range(3, 96*365*4, 150)),
+                },
+            ],
+        })
 
-        cls.cm_ng = models.ConsumptionMetadata.objects.create(
-            project=cls.project,
-            interpretation="NG_C_S",
-            unit="THM",
-        )
-
-        cls.cm_e = models.ConsumptionMetadata.objects.create(
-            project=cls.project,
-            interpretation="E_C_S",
-            unit="KWH",
-        )
-
-        records = []
-
-        for i in range(0, 700, 30):
-            if i % 120 == 0:
-                value = np.nan
-            else:
-                value = 1.0
-            records.append(models.ConsumptionRecord(
-                metadata=cls.cm_ng,
-                start=datetime(2011, 1, 1, tzinfo=pytz.UTC) + timedelta(days=i),
-                value=value,
-                estimated=False,
-            ))
-
-        for i in range(0, 6000):
-            if i % 4 == 0:
-                value = np.nan
-            else:
-                value = 1
-            records.append(models.ConsumptionRecord(
-                metadata=cls.cm_e,
-                start=datetime(2011, 12, 1, tzinfo=pytz.UTC) + timedelta(seconds=i*900),
-                value=value,
-                estimated=False,
-            ))
-
-        models.ConsumptionRecord.objects.bulk_create(records)
+        cls.project.run_meter()
 
         tmp_dir = tempfile.mkdtemp()
         wns = TMY3WeatherSource("724838", tmp_dir, preload=False)
@@ -108,8 +129,7 @@ class ProjectTestCase(TestCase):
             assert hasattr(self.project, attribute)
 
     def test_lat_lng(self):
-        lat_lng = self.project.lat_lng
-        assert len(lat_lng) == 2
+        assert self.project.lat_lng is None
 
     def test_eemeter_project(self):
 
@@ -126,7 +146,7 @@ class ProjectTestCase(TestCase):
         assert isinstance(project_result.project_id, int)
         assert project_result.meter_settings is None
 
-        assert len(project_result.derivative_aggregations.all()) == 3
-        assert len(project_result.energy_trace_model_results.all()) == 4
+        assert len(project_result.derivative_aggregations.all()) == 4
+        assert len(project_result.energy_trace_model_results.all()) == 12
         assert len(project_result.modeling_periods.all()) == 2
         assert len(project_result.modeling_period_groups.all()) == 1

datastore/tests/views/test_project_result.py
@@ -1,11 +1,15 @@
 from .shared import OAuthTestCase
 from django.contrib.auth.models import User
-from datastore import models
+
 from datetime import datetime, timedelta
-import pytz
 import json
+
+import pytz
 import numpy as np
 from numpy.testing import assert_allclose
+
+from datastore import models
+from datastore.services import create_project
 
 class ProjectAPITestCase(OAuthTestCase):
 
@@ -13,58 +17,75 @@ class ProjectAPITestCase(OAuthTestCase):
     def setUpTestData(cls):
         super(ProjectAPITestCase, cls).setUpTestData()
 
-        cls.project = models.Project.objects.create(
-            project_owner=cls.user.projectowner,
-            project_id="PROJECTID",
-            baseline_period_start=None,
-            baseline_period_end=datetime(2012, 1, 1, tzinfo=pytz.UTC),
-            reporting_period_start=datetime(2012, 1, 2, tzinfo=pytz.UTC),
-            reporting_period_end=None,
-            zipcode="91104",
-            weather_station="722880",
-            latitude=0,
-            longitude=0,
-        )
-
-        cls.cm_ng = models.ConsumptionMetadata.objects.create(
-            project=cls.project,
-            interpretation="NG_C_S",
-            unit="THM",
-        )
-
-        cls.cm_e = models.ConsumptionMetadata.objects.create(
-            project=cls.project,
-            interpretation="E_C_S",
-            unit="KWH",
-        )
-
-        records = []
-
-        for i in range(0, 700, 30):
-            if i % 120 == 0:
-                value = np.nan
-            else:
-                value = 1.0
-            records.append(models.ConsumptionRecord(
-                metadata=cls.cm_ng,
-                start=datetime(2011, 1, 1, tzinfo=pytz.UTC) + timedelta(days=i),
-                value=value,
-                estimated=False,
-            ))
-
-        for i in range(0, 6000):
-            if i % 4 == 0:
-                value = np.nan
-            else:
-                value = 1
-            records.append(models.ConsumptionRecord(
-                metadata=cls.cm_e,
-                start=datetime(2011, 12, 1, tzinfo=pytz.UTC) + timedelta(seconds=i*900),
-                value=value,
-                estimated=False,
-            ))
-
-        models.ConsumptionRecord.objects.bulk_create(records)
+        cls.project = create_project(spec={
+            "project_id": "BCDE",
+            "project_owner": cls.project_owner,
+            "baseline_period_end": datetime(2012, 1, 1, tzinfo=pytz.UTC),
+            "reporting_period_start": datetime(2012, 2, 1, tzinfo=pytz.UTC),
+            "zipcode": "91104",
+            "traces": [
+                {
+                    "interpretation": "NG_C_S",
+                    "unit": "THM",
+                    "start": "2010-01-01",
+                    "end": "2014-12-31",
+                    "freq": "MS",
+                    "value": 1,
+                    "nans": set(range(0, 60, 20)),
+                    "estimated": set(range(3, 60, 15)),
+                },
+                {
+                    "interpretation": "NG_C_S",
+                    "unit": "THM",
+                    "start": "2011-09-01",
+                    "end": "2014-12-31",
+                    "freq": "D",
+                    "value": 2,
+                    "nans": set(range(0, 1000, 20)),
+                    "estimated": set(range(3, 1000, 15)),
+                },
+                {
+                    "interpretation": "E_C_S",
+                    "unit": "KWH",
+                    "start": "2011-01-01",
+                    "end": "2014-12-31",
+                    "freq": "15T",
+                    "value": 0.04,
+                    "nans": set(range(0, 96*365*4, 200)),
+                    "estimated": set(range(3, 96*365*4, 150)),
+                },
+                {
+                    "interpretation": "E_C_S",
+                    "unit": "KWH",
+                    "start": "2011-01-01",
+                    "end": "2014-12-31",
+                    "freq": "H",
+                    "value": 0.4,
+                    "nans": set(range(0, 96*365*4, 200)),
+                    "estimated": set(range(3, 96*365*4, 150)),
+                },
+                {
+                    "interpretation": "E_OSG_U",
+                    "unit": "KWH",
+                    "start": "2012-01-15",
+                    "end": "2014-12-31",
+                    "freq": "H",
+                    "value": 0.3,
+                    "nans": set(range(0, 96*365*4, 200)),
+                    "estimated": set(range(3, 96*365*4, 150)),
+                },
+                {
+                    "interpretation": "E_OSG_U",
+                    "unit": "KWH",
+                    "start": "2010-01-01",
+                    "end": "2014-12-31",
+                    "freq": "30T",
+                    "value": 0.1,
+                    "nans": set(range(0, 96*365*4, 200)),
+                    "estimated": set(range(3, 96*365*4, 150)),
+                },
+            ],
+        })
         cls.project.run_meter()
 
     def test_project_result_read(self):