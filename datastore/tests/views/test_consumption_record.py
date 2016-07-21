a32d1261fd4487e4707b02cd3ee9490958fa0a5a
Marc Pare <marcpare0@gmail.com>
2016-07-21 16:01:23 -0700

    Merge conflicts snuck through

--- datastore/tests/views/test_consumption_record.py
+++ datastore/tests/views/test_consumption_record.py
@@ -6,15 +6,7 @@ from datastore import models
 class ConsumptionRecordAPITestCase(OAuthTestCase):
 
     def setUp(self):
-<<<<<<< 9ea5744407ab3b4c783d4083a682e5729e2377d8
-        """
-        Setup methods for a eemeter run storage
-        engine.
-        """
-        super(ConsumptionRecordAPITestCase, self).setUp()
-=======
         super(ConsumptionRecordAPITestCase,self).setUp()
->>>>>>> Update API routes and tests to include `CM.label` field
 
 
     def test_consumption_record_sync(self):
@@ -197,13 +189,8 @@ class ConsumptionRecordAPITestCase(OAuthTestCase):
         assert isinstance(response.data[0]['id'], int)
         assert response.data[0]['start'] == '2014-01-01T00:00:00Z'
         assert response.data[0]['value'] == 0.0
-<<<<<<< 9ea5744407ab3b4c783d4083a682e5729e2377d8
-        assert response.data[0]['estimated'] is False
-        assert response.data[0]['metadata'] == self.consumption_metadata.pk
-=======
         assert response.data[0]['estimated'] == False
         assert response.data[0]['metadata'] == cm_id
->>>>>>> Update API routes and tests to include `CM.label` field
 
         consumption_record_id = response.data[0]['id']
         response = self.get(
@@ -214,11 +201,6 @@ class ConsumptionRecordAPITestCase(OAuthTestCase):
         assert isinstance(response.data['id'], int)
         assert response.data['start'] == '2014-01-01T00:00:00Z'
         assert response.data['value'] == 0.0
-<<<<<<< 9ea5744407ab3b4c783d4083a682e5729e2377d8
-        assert response.data['estimated'] is False
-        assert response.data['metadata'] == self.consumption_metadata.pk
-=======
         assert response.data['estimated'] == False
         assert response.data['metadata'] == cm_id
 
->>>>>>> Update API routes and tests to include `CM.label` field
