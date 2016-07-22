ee17776a2c56bc2a027d23c547e969474e76ca8f
Phil Ngo <ngo.phil@gmail.com>
2016-07-21 17:45:27 -0700

    Bump version

--- oeem_energy_datastore/__init__.py
+++ oeem_energy_datastore/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from .celery import app as celery_app  # noqa
 
 __title__ = 'Open Energy Efficiency Meter Datastore'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'Phil Ngo and Marc Pare'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2011-2016 Open Energy Efficiency'
