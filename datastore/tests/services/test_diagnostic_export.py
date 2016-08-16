faa45c3b995f2a1d8b2e03ed70d95e8678c1cdc3
Phil Ngo <ngo.phil@gmail.com>
2016-08-15 17:41:55 -0700

    Update header n

--- datastore/tests/services/test_diagnostic_export.py
+++ datastore/tests/services/test_diagnostic_export.py
@@ -62,7 +62,7 @@ class DiagnosticExportServiceTestCase(TestCase):
     def test_basic_usage(self):
         result = diagnostic_export()
         headers = result['headers']
-        assert len(headers) == 37
+        assert len(headers) == 47
 
         rows = result['rows']
         assert len(rows) == 1
